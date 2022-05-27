8a7c79327a22926ac1582403937c1b8ca1228e51
franco aller <franco.e.aller@gmail.com>
2022-05-27 23:19:40 +0000

    1# - se agrega maquetado incial

--- src/App.jsx
+++ src/App.jsx
@@ -2,43 +2,94 @@ import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0);
+function App() { 
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React222222222222222222222222!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <div ClassName="layout">
+    {/*Cabecera*/}
+    <header ClassName="header">
+        <div ClassName="logo">
+            <div ClassName="play"></div>
+        </div>
+        
+        <h1>MisPelis</h1>
+    </header>
+
+    {/*Barra de navegación*/}
+    <nav ClassName="nav">
+        <ul>
+            <li><a href="#">Inicio</a></li>
+            <li><a href="#">Peliculas</a></li>
+            <li><a href="#">Blog</a></li>
+            <li><a href="#">Contacto</a></li>
+        </ul>
+    </nav>
+
+    {/*Contenido principal*/}
+    <section id="content" ClassName="content">
+
+        {/*aqui van las peliculas*/}
+        <article ClassName="peli-item">
+            <h3 ClassName="title">Desarrollo web</h3>
+            <p ClassName="description">victorroblesweb.es</p>
+
+            <button ClassName="edit">Editar</button>
+            <button ClassName="delete">Borrar</button>
+        </article>
+
+        <article ClassName="peli-item">
+            <h3 ClassName="title">Desarrollo web</h3>
+            <p ClassName="description">victorroblesweb.es</p>
+
+            <button ClassName="edit">Editar</button>
+            <button ClassName="delete">Borrar</button>
+        </article>
+
+        <article ClassName="peli-item">
+            <h3 ClassName="title">Desarrollo web</h3>
+            <p ClassName="description">victorroblesweb.es</p>
+
+            <button ClassName="edit">Editar</button>
+            <button ClassName="delete">Borrar</button>
+        </article>
+
+        <article ClassName="peli-item">
+            <h3 ClassName="title">Desarrollo web</h3>
+            <p ClassName="description">victorroblesweb.es</p>
+
+            <button ClassName="edit">Editar</button>
+            <button ClassName="delete">Borrar</button>
+        </article>
+
+    </section>
+
+    {/*Barra lateral*/}
+    <aside ClassName="lateral">
+        <div ClassName="search">
+            <h3 ClassName="title">Buscador</h3>
+            <form>
+                <input type="text" id="search_field" />
+                <button id="search">Buscar</button>
+            </form>
+        </div>
+
+        <div ClassName="add">
+            <h3 ClassName="title">Añadir pelicula</h3>
+            <form>
+                <input type="text" id="title" placeholder="Titulo" />
+                <textarea id="description" placeholder="Descripción"></textarea>
+                <input type="submit" id="save" value="Guardar" />
+            </form>
+        </div>
+    </aside>
+
+    {/*Pie de página*/}
+    <footer ClassName="footer">
+        &copy; Máster en React - <a href="https://victorroblesweb.es">victorroblesweb.es</a>
+    </footer>
+
+</div>
+
   );
 }
 
